ca8ecd694e57e5bc02a6e7f2de1dec190643e9f7
Marinadelgado88 <marinadelges@gmail.com>
2021-06-08 20:22:56 +0200

    styled Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,27 @@
 import '../stylesheets/components/Footer.scss';
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <small className='footer__text'>2021 @Marinadelgado</small>
-      <nav>
-        <a
-          href='https://github.com/Marinadelgado88'
-          className='footer__github'
-          title='GitHub'
-          target='GitHub'
-        >
-          <i className='fab fa-github footer__github--icon'></i>
-        </a>
-        <a
-          href='https://twitter.com/_MarinaDelgado_'
-          className='footer__twitter'
-          title='Twitter'
-          target='Twitter'
-        >
-          <i className='fab fa-twitter footer__twitter--icon'></i>
-        </a>
-        <a
-          href='https://www.linkedin.com/in/marinadelgadoesquinas/'
-          className='footer__linkedin'
-          title='Linkedin'
-          target='Linkedin'
-        >
-          <i className='fab fa-linkedin footer__linkedin--icon'></i>
-        </a>
-      </nav>
-    </footer>
+    <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <ul class="social">
+                    
+                    <li><a href="https://twitter.com/_MarinaDelgado_"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/marinadelgadoesquinas/"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://github.com/Marinadelgado88"><i class="fa fa-rss"></i></a></li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="copyright">Copyright &copy; 2021 @Marinadelgado</p> 
+            </div>
+        </div>
+    </div>
+</footer>
+
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
-
+import '../stylesheets/components/Header.scss';
 const Header = props =>{
 
   
   return (
-    <section>
-      <ul className="cards">
-        
-      </ul>
-    </section>
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                      
+                        <ul class="nav">
+                            <li><a href="#welcome" class="active">Home</a></li>
+                            
+                            <li><a href="#work-process">Ejercicios</a></li>
+                            <li><a href="#testimonials">Video</a></li>
+                            <li><a href="#contact-us">Contact Me</a></li>
+                        </ul>
+                       
+                            <span>Menu</span>
+                       
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 export default Header;
